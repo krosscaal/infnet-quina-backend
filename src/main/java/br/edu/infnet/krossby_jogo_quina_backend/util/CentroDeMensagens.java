5c212ff95bc7bd66ead07dfbc7cdfb3376bc77ff
Krossby <krosscaal@gmail.com>
2026-01-16 12:20:11 -0300

    feat: add centro de mensagens

---
+++ src/main/java/br/edu/infnet/krossby_jogo_quina_backend/util/CentroDeMensagens.java
@@ -0,0 +1,19 @@
+/*
+ * Author: Krossby Adhemar Camacho Alviz
+ *
+ */
+
+package br.edu.infnet.krossby_jogo_quina_backend.util;
+
+public class CentroDeMensagens {
+    private CentroDeMensagens(){}
+
+    public static final String NAO_NULL = "Não pode ser nulo!";
+    public static final String NAO_VAZIO = "Não pode ser vazio!";
+    public static final String E_UM_CAMPO_OBRIGATORIO = "é um campo obrigatorio!";
+    public static final String FORMATO_E_MAIL_INCORRETO = "e-mail com formato incorreto (dever estar no formato: xxxxxxxxxx@xxxxxxx.xxxx exemplo: nomeUsuario@dominio.com)!";
+    public static final String DEVE_CONTER_SOMENTE_NUMEROS = "deve conter apenas dígitos numéricos!";
+    public static final String APOSTA_PRECISA_SER_INFORMADA = "Aposta precisa ser informada!";
+    public static final String FALTA_NUMEROS_EM_APOSTA = "Aposta sem quantidade de numeros requerida!";
+
+}
